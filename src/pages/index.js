4038cde1596f36096d820f2193ddb90b88691fd5
Lucas Luz <lucasluzthatsme@gmail.com>
2021-02-18 11:02:38 -0300

    feat: add MainContainer component to center content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { MainContainer } from '../components/main-container'
 import GlobalStyle from '../styles/global'
 function IndexPage () {
-  return <>
-    <GlobalStyle/>
-    <h1>Oi</h1>
-  </>
+  return <MainContainer>
+       <GlobalStyle/>
+       <h1>oi</h1>
+  </MainContainer>
 }
 
 export default IndexPage
