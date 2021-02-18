64831965fb533c1865884ebe401f298a77d9c6e9
Lucas Luz <lucasluzthatsme@gmail.com>
2021-02-18 10:53:02 -0300

    feat: add global style

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import GlobalStyle from '../styles/global'
+function IndexPage () {
+  return <>
+    <GlobalStyle/>
+    <h1>Oi</h1>
+  </>
+}
+
+export default IndexPage
