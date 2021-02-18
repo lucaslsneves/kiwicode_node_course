3bb4c7aecaf3f6690538195c0ece7b946c471dbe
Lucas Luz <lucasluzthatsme@gmail.com>
2021-02-18 20:31:49 -0300

    feat: add media query to h1 in AboutSection title

--- src/components/AboutSection/styles.js
+++ src/components/AboutSection/styles.js
@@ -37,6 +37,12 @@ export const Content = styled.div`
 export const Title = styled.div`
   border-left: 5px solid #00d56f;
   padding-left:15px;
+  @media(max-width:1180px) {
+  
+     h1 {
+      font-size:23px !important;
+    }
+}
   h1 {
     color: #222;
     font-size: 26px;
