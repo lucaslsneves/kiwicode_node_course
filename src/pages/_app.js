19d30e2f6215f455e76b00df340fde7a0f6cd6c3
Lucas Luz <lucasluzthatsme@gmail.com>
2021-03-02 09:50:33 -0300

    feat: add icon and seo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,30 @@
 import '../styles/_app.css'
-
+import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Head>
+        <link rel="shortcut icon" href="/kiwi-yout.png" />
+        <link rel="apple-touch-icon" href="/kiwi-yout.png" />
+      </Head>
+    <NextSeo
+        title="Kiwi Code - Curso Clean Architecture NodeJs"
+        description="Aprenda criar uma API com NodeJs e Typescript utilizando boas práticas como: Clean Architecture,SOLID e TDD"
+        canonical="https://kiwicode.com.br/"
+        openGraph={{
+          url: 'https://kiwicode.com.br/',
+          title:
+            'Kiwi Code - Curso Clean Architecture NodeJs',
+          description:
+            'Aprenda a criar uma API com NodeJs e Typescript utilizando boas práticas como: Clean Architecture,SOLID e TDD',
+          site_name: 'Kiwi Code',
+          locale: 'pt_BR'
+        }}
+      />
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 // Only uncomment this method if you have blocking data requirements for
